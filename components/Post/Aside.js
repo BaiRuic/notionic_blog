748b62095f6fff9d0a7fbd0fd6385e617460f9d4
Bairuic <brc730@outlook.com>
2023-01-27 17:13:23 +0800

    注释了post右边栏的赞助按钮

--- components/Post/Aside.js
+++ components/Post/Aside.js
@@ -27,14 +27,14 @@ const Aside = ({ subPageTitle, frontMatter }) => {
       <aside className='hidden sticky md:flex md:flex-col md:items-center md:self-start md:ml-8 md:inset-y-1/2'>
         <div className='flex flex-col items-center text-center'>
           <div className='bg-gray-100 dark:bg-gray-700 grid rounded-lg block p-2 gap-y-5 nav'>
-            {BLOG.showWeChatPay && (
+            {/* {BLOG.showWeChatPay && (
               <button
                 onClick={() => setShowPay((showPay) => !showPay)}
                 className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'
               >
                 <ThumbUpIcon className='w-5 h-5' />
               </button>
-            )}
+            )}  */}
             {showSubPageTitle && (
               <Link passHref href={`${BLOG.path}/${frontMatter.slug}`} scroll={false}>
                 <a className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'>
@@ -55,7 +55,7 @@ const Aside = ({ subPageTitle, frontMatter }) => {
           </div>
         </div>
       </aside>
-      {showPay && <WechatPay />}
+      {/* {showPay && <WechatPay />}  */}
       {showButton && (
         <button
           onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
