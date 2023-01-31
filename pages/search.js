7320e08b3eeecd2833941022ac0ef94f00b18876
Bairuic <brc730@outlook.com>
2023-01-31 13:26:03 +0800

    修改了search页面，使得index, notes，books等不会出现在搜索页面

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,20 @@
 import { getAllPosts, getAllTagsFromPosts } from '@/lib/notion'
 import SearchLayout from '@/layouts/search'
+import filterSearchedPosts from './filterPublishedPosts'
 
 export default function search({ tags, posts }) {
   return <SearchLayout tags={tags} posts={posts} />
 }
 export async function getStaticProps() {
-  const posts = await getAllPosts({ onlyNewsletter: false })
+  // 只能搜索 notionPageID下的 页面。
+  const all_posts = await getAllPosts({ onlyNewsletter: false })
+  const excludeSlug = ['index', 'newsletter', 'notes', 'about', 'projects', 'friend', 'books']
+  // 移除不需要显示在搜索页面的 post
+  const posts = filterSearchedPosts({
+    posts: all_posts,
+    excludeSlug: excludeSlug
+  })
+
   const tags = getAllTagsFromPosts(posts)
   return {
     props: {
